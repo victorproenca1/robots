4286a2a1037e074409a1c64189424dcd6f40e247
victorproenca1 <victorproenca2020@gmail.com>
2021-06-07 22:42:01 -0300

    atualizando projeto (jsonplaceholder e pastas)

--- src/robots.js
+++ src/robots.js
@@ -1,25 +1,25 @@
 export const robots = [
     {
         id: 1,
-        nome: 'Mechanized First Aid Bot',
+        name: 'Mechanized First Aid Bot',
         username: 'first-aid-bot',
         email: 'firstaidbot@gmail.com'
     },
     {
         id: 2,
-        nome: 'Artificial Encryption Juggernaut',
+        name: 'Artificial Encryption Juggernaut',
         username: 'encryption',
         email: 'encryption@gmail.com'
     },
     {
         id: 3,
-        nome: 'Extra-Terrestrial Human Protection Cyborg',
+        name: 'Extra-Terrestrial Human Protection Cyborg',
         username: 'cyborg',
         email: 'cyborg@gmail.com'
     },
     {
         id: 4,
-        nome: 'Compact Life Protection Device',
+        name: 'Compact Life Protection Device',
         username: 'protection-device',
         email: 'protectiondevice@gmail.com'
     },
