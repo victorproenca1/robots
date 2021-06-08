7583c741046a4c720da0f00a9a0f66c3b21db830
victorproenca1 <victorproenca2020@gmail.com>
2021-06-07 21:00:05 -0300

    primeiro commit

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Card from './Card';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Card />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        nome: 'Mechanized First Aid Bot',
+        username: 'first-aid-bot',
+        email: 'firstaidbot@gmail.com'
+    },
+    {
+        id: 2,
+        nome: 'Artificial Encryption Juggernaut',
+        username: 'encryption',
+        email: 'encryption@gmail.com'
+    },
+    {
+        id: 3,
+        nome: 'Extra-Terrestrial Human Protection Cyborg',
+        username: 'cyborg',
+        email: 'cyborg@gmail.com'
+    },
+    {
+        id: 4,
+        nome: 'Compact Life Protection Device',
+        username: 'protection-device',
+        email: 'protectiondevice@gmail.com'
+    },
+]
